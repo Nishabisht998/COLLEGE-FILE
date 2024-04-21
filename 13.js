d469b546d444ac21c1346e5ad920cf4235d3a771
Nishabisht998 <136101943+Nishabisht998@users.noreply.github.com>
2024-04-21 23:56:42 +0530

    Add files via upload

---
+++ 13.js
@@ -0,0 +1,6 @@
+let menuButton = document.getElementById("menu-button"); 
+let menu = document.getElementById("menu"); 
+ 
+menuButton.addEventListener("click", function() { 
+  menu.classList.toggle("show-menu"); 
+}); 
