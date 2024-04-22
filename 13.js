ff0cbe179bf2f9ff0e9f1adcd596a24bcaebae42
Nishabisht998 <136101943+Nishabisht998@users.noreply.github.com>
2024-04-22 21:06:44 +0530

    Delete 13.js

--- 13.js
+++
@@ -1,6 +0,0 @@
-let menuButton = document.getElementById("menu-button"); 
-let menu = document.getElementById("menu"); 
- 
-menuButton.addEventListener("click", function() { 
-  menu.classList.toggle("show-menu"); 
-}); 
